987c6dd94375f68b20bd08348f22bfc893942215
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-09-25 21:16:37 +0530

    modified style sheet

--- src/App.js
+++ src/App.js
@@ -21,48 +21,40 @@ function App() {
   };
 
   return (
-    <div className=" main flex justify-center flex-col gap-5 items-center">
-      <div className="container w-[24rem] gap-8 rounded-lg p-5 flex flex-col justify-center items-center">
-        <header className=" text-2xl font-semibold">
-          Is your birthday lucky??
-        </header>
-        <div className=" flex flex-col w-[90%] gap-5 ">
-          <div className=" flex flex-col">
+    <div className=" main">
+      <div className="container">
+        <header className=" heading ">Is your birthday lucky??</header>
+        <div className=" input__block">
+          <div className=" input__label">
             <label htmlFor="dob">Date of Birth</label>
             <input
               type="date"
               id="dob"
               value={dob}
               onChange={(e) => setDob(e.target.value)}
-              className=" p-2 rounded-lg"
+              className="input__style"
             />
           </div>
-          <div className=" flex flex-col">
+          <div className=" input__label">
             <label htmlFor="luckyNumber">Your lucky number</label>
             <input
               type="number"
               id="luckyNumber"
               value={luckyNumber}
               onChange={(e) => setLuckyNumber(e.target.value)}
-              className=" p-2 rounded-lg"
+              className="input__style"
             />
           </div>
         </div>
-        <button
-          className=" border-2 border-white px-3 py-1 rounded-lg hover:text-white transition-all ease-in-out duration-150 hover:border-gray-500"
-          onClick={() => checkMyLuck(dob, luckyNumber)}
-        >
+        <button className=" btn " onClick={() => checkMyLuck(dob, luckyNumber)}>
           Try Your Luck
         </button>
-        <button
-          className=" border-2 border-white px-3 py-1 rounded-lg hover:text-white transition-all ease-in-out duration-150 hover:border-gray-500"
-          onClick={() => window.location.reload()}
-        >
+        <button className=" btn" onClick={() => window.location.reload()}>
           Reset
         </button>
-        <p className=" text-center text-xl">{result}</p>
+        <p className=" result">{result}</p>
       </div>
-      <div className="social-icons flex items-center justify-center gap-5 text-2xl ">
+      <div className="social__icons  ">
         <p className="footer">Copyright &copy; 2022 - Gurudatt Puranik</p>
         <a href="https://twitter.com/PuranikGurudatt">
           <i className="fa-brands fa-twitter"></i>
